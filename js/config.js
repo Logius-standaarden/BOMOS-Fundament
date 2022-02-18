a837792af9ad4322078d3fa538426f81eb4ac5a8
Edwin Wisse <edwin@wisze.net>
2022-02-18 10:15:51 +0100

    Titel
    Titel

--- js/config.js
+++ js/config.js
@@ -52,23 +52,23 @@ var respecConfig = {
     {
       name: "Sander Boer",
       company: "Logius",
-      companyURL: "http://www.logius.nl/",
+      companyURL: "https://www.logius.nl/",
     },
     {
       name: "Erwin Kaats",
       company: "Logius",
-      companyURL: "http://www.logius.nl/",
+      companyURL: "https://www.logius.nl/",
     },
     {
       name: "Edwin Wisse",
       company: "Logius",
-      companyURL: "http://www.logius.nl/",
+      companyURL: "https://www.logius.nl/",
     }
   ],
   // shortName = korte naam voor in de url van het document, gebruik alleen letters, cijfers en eventueel '-'
   shortName: "bomos",
   // url van de github repository waar je in werkt
-  github: "https://github.com/Logius-standaarden/BOMOS",
+  github: "https://github.com/Logius-standaarden/BOMOS-Fundament",
   // Controls if linked "§" section markers are added to a document
   addSectionLinks: true,
   // url van de issue lijst in de github repository waar je in werkt
