fa70323f096e90a3c57b596fd965f4d3f05de459
Stas Mironov <stas.mironov@logius.nl>
2025-10-06 16:10:45 +0200

    Fix pdf name error

--- js/config.mjs
+++ js/config.mjs
@@ -1,10 +1,6 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
 
 loadRespecWithConfiguration({
-  alternateFormats: [ { 
-        "label" : "pdf",
-        "uri" : "BOMOS-Fundament.pdf"
-      } ],
   authors: [ { 
         "company" : "HAN University of Applied Sciences",
         "companyURL" : "https://www.han.nl/",
