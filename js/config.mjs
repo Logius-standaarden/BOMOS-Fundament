002120bc8b74854aa02662836e0bbfe15acee935
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2026-07-17 09:43:14 +0200

    Beheer: fix companyURL met trailing slash

--- js/config.mjs
+++ js/config.mjs
@@ -9,22 +9,22 @@ loadRespecWithConfiguration({
   editors: [ 
       { 
         "company" : "HAN University of Applied Sciences",
-        "companyURL" : "https://www.han.nl/",
+        "companyURL" : "https://www.han.nl",
         "name" : "Erwin Folmer"
       },
       { 
         "company" : "Logius",
-        "companyURL" : "https://www.logius.nl/",
+        "companyURL" : "https://www.logius.nl",
         "name" : "Gül Işik"
       },
       { 
         "company" : "Logius",
-        "companyURL" : "https://www.logius.nl/",
+        "companyURL" : "https://www.logius.nl",
         "name" : "Edwin Wisse"
       },
       { 
         "company" : "TNO",
-        "companyURL" : "https://www.tno.nl/nl/",
+        "companyURL" : "https://www.tno.nl/nl",
         "name" : "Wouter van den Berg"
       }
     ],
