400d106c6528fbfb1242a213d03c3929558945e9
Tim van der Lippe <Tim.Lippe@logius.nl>
2026-08-24 16:05:49 +0200

    Voeg base config toe

--- js/config.mjs
+++ js/config.mjs
@@ -1,41 +1,8 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
+import { baseConfig } from "./base-config.mjs";
 
 loadRespecWithConfiguration({
-  authors: [ { 
-        "company" : "HAN University of Applied Sciences",
-        "companyURL" : "https://www.han.nl/",
-        "name" : "Erwin Folmer"
-      } ],
-  editors: [ 
-      { 
-        "company" : "HAN University of Applied Sciences",
-        "companyURL" : "https://www.han.nl/",
-        "name" : "Erwin Folmer"
-      },
-      { 
-        "company" : "Logius",
-        "companyURL" : "https://www.logius.nl",
-        "name" : "Gül Işik"
-      },
-      { 
-        "company" : "Logius",
-        "companyURL" : "https://www.logius.nl",
-        "name" : "Edwin Wisse"
-      },
-      { 
-        "company" : "TNO",
-        "companyURL" : "https://www.tno.nl/nl/",
-        "name" : "Wouter van den Berg"
-      }
-    ],
-  github: "https://github.com/Logius-standaarden/BOMOS-Fundament",
-  maxTocLevel: 3,
-  previousPublishVersion: "3.0.0",
-  pubDomain: "bomos",
-  publishDate: "2023-11-02",
-  publishVersion: "3.0.2",
+  ...baseConfig,
   shortName: "fundament",
-  specStatus: "WV",
-  specType: "HR",
   spellcheck: false,
 });
